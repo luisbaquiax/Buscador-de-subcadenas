db6ad0ffe9ff4b0dec9556513d9cafbef4b96558
luisbaquiax <luis.baquiax1995@gmail.com>
2020-07-26 21:58:07 -0600

    s

--- Tarea1/src/com/enCosola/ADN.java
+++ Tarea1/src/com/enCosola/ADN.java
@@ -45,8 +45,8 @@ public class ADN {
         char[] b = adn2.toCharArray();
         String patronRepetido = "";
 
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = 0; j < b.length - 1; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
                 if (a[i] == b[j]) {
                     patronRepetido += a[i];
                 }
