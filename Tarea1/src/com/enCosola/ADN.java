de5dd57ed1fcd9a1f919ac3a01c198980bddc0a5
luisbaquiax <luis.baquiax1995@gmail.com>
2020-07-26 21:04:46 -0600

    intento n-k

--- Tarea1/src/com/enCosola/ADN.java
+++ Tarea1/src/com/enCosola/ADN.java
@@ -26,8 +26,6 @@ public class ADN {
     }
 
     public ADN() {
-//        arreglo1 = new String[100];
-//        arreglo2 = new String[100];
 
         System.out.print("INgrese el primer ADN: ");
         adn1 = scanner.next();
@@ -37,63 +35,25 @@ public class ADN {
         char[] c1 = adn1.toCharArray();
         char[] c2 = adn2.toCharArray();
 
-        for (int i = 0; i < c1.length; i++) {
-
-            insertarCadena(verifcarPatronRepetido(c1[i] + "", i, c1), arreglo1);
-
-        }
-        System.out.println("*******************");
-        for (int i = 0; i < c2.length; i++) {
-            insertarCadena(verifcarPatronRepetido(c2[i] + "", i, c2), arreglo2);
-        }
-        String patronReptido = "";
-        System.out.println("++++++ arreglod 1");
-        for (int i = 0; i < arreglo1.length; i++) {
-            if (this.arreglo1[i] != null) {
-                System.out.println(arreglo1[i]);
-            }
-
-        }
-        System.out.println("++++++ arreglod 2");
-        for (int i = 0; i < arreglo2.length; i++) {
-            if (this.arreglo1[i] != null) {
-                System.out.println(arreglo1[i]);
-            }
-
-        }
-        System.out.println("fin ");
-        for (int i = 0; i < arreglo1.length; i++) {
-            for (int j = 0; j < arreglo2.length; j++) {
-
-                if (arreglo1[i].equals(arreglo2[j])) {
-                    arreglo1[i] = patronReptido;
-                    System.out.println(patronReptido);
-                    return;
-
-                }
-
-            }
-
-        }
-
+     
+       
     }
 
-    public String verifcarPatronRepetido(String caracter, int indice, char[] cadena) {
+    public void verifcarPatronRepetido(String caracter, int indice, char[] cadena) {
         String patronRepetido = "";
         for (int i = indice; i < cadena.length; i++) {
             String combinacion = caracter + cadena[i];
             System.out.println(combinacion);
-            return combinacion;
-            verifcarPatronRepetido(combinacion, indice++, cadena);
-            
+            insertarCadena(combinacion, this.arreglo1);
+            verifcarPatronRepetido(combinacion, ++indice, cadena);
         }
-        return patronRepetido;
     }
 
     public void insertarCadena(String cad, String[] cadena) {
         for (int i = 0; i < cadena.length; i++) {
             if (cadena[i] == null) {
                 cadena[i] = cad;
+                return;
             }
 
         }
