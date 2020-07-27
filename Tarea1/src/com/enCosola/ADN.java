1dda378c4202acf538026439879541534ec37544
luisbaquiax <luis.baquiax1995@gmail.com>
2020-07-27 01:52:57 -0600

    Por fin salió

--- Tarea1/src/com/enCosola/ADN.java
+++ Tarea1/src/com/enCosola/ADN.java
@@ -35,65 +35,76 @@ public class ADN {
         char[] c1 = adn1.toCharArray();
         char[] c2 = adn2.toCharArray();
 
-        //System.out.println("Patron repetido " + verifcarPatronRepetido(adn1, adn2));
-        for (int i = 0; i < c1.length; i++) {
-            verifcarCombinaciones(c1[i] + "", i, c1, arreglo1);
-
-        }
-        for (int i = 0; i < c2.length; i++) {
-            verifcarCombinaciones(c2[i] + "", i, c2, arreglo2);
-
-        }
-        System.out.println("CADENA: " + cadenaRepetida(arreglo1, adn2));
+        System.out.println("Patron repetido " + verificarPatronRepetido(adn1, adn2));
 
     }
 
-    public String repiticion(String[] arreglo1, String[] arreglo2) {
-        String repetido = "";
+    /**
+     * Método que se encarga de verificar si hay algún patrón repetido
+     *
+     * @param cadena1
+     * @param cadena2
+     * @return
+     */
+    public String verificarPatronRepetido(String cadena1, String cadena2) {
+        
+        int m = cadena1.length();
+        int n = cadena2.length();
+        //Guardar las longitudes mas largas
+        int[][] longitudes = new int[m + 1][n + 1];
 
-        for (int i = 0; i < arreglo1.length; i++) {
-            if (arreglo1[i] != null) {
-                for (int j = 0; j < arreglo2.length; j++) {
-                    if (arreglo2[j] != null && (arreglo1[i].equals(arreglo2[j])) && (arreglo1[i].length() > arreglo2[j].length())) {
-                        return arreglo1[i];
+        //la longitud mas grande y común entre las dos cadenas
+        int masGrande = 0;
+
+        int fila = 0;
+        int columna = 0;
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    longitudes[i][j] = 0;
+                } else if (cadena1.charAt(i - 1) == cadena2.charAt(j - 1)) {
+                    longitudes[i][j] = longitudes[i - 1][j - 1] + 1;
+                    if (masGrande < longitudes[i][j]) {
+                        masGrande = longitudes[i][j];
+                        fila = i;
+                        columna = j;
                     }
+                } else {
+                    longitudes[i][j] = 0;
                 }
             }
-
         }
-        return repetido;
-    }
 
-    public String cadenaRepetida(String[] cadena, String ad2) {
-        String masGrande = "";
-        for (int i = 0; i < cadena.length; i++) {
-            if (ad2.contains(cadena[i]) && (cadena[i] != null)) {
-               return cadena[i];
-            }
-
+        String noHay = "";
+        if (masGrande == 0) {
+            System.out.println("NO HAY PATRON REPETIDO");
+            return noHay;
         }
-        return masGrande;
-    }
 
-    public String verifcarPatronRepetido(String adn1, String adn2) {
-        char[] a = adn1.toCharArray();
-        char[] b = adn2.toCharArray();
         String patronRepetido = "";
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                if (a[i] == b[j]) {
-                    patronRepetido += a[i];
-                }
-
-            }
-
+        while (longitudes[fila][columna] != 0) {
+            patronRepetido = cadena1.charAt(fila - 1) + patronRepetido;
+            --masGrande;
+            fila--;
+            columna--;
         }
+
+        System.out.println(patronRepetido);
         return patronRepetido;
     }
 
+    /**
+     * Método que se encarga de verificar las combinaciones posibles de una
+     * cadena
+     *
+     * @param caracter
+     * @param indice
+     * @param cadena
+     * @param arreglo
+     */
     public void verifcarCombinaciones(String caracter, int indice, char[] cadena, String[] arreglo) {
-       
+
         for (int i = indice; i < cadena.length; i++) {
             String combinacion = caracter + cadena[i];
             System.out.println(combinacion);
@@ -102,6 +113,12 @@ public class ADN {
         }
     }
 
+    /**
+     * Me sirvió para guardar una combinación
+     *
+     * @param cad
+     * @param cadena
+     */
     public void insertarCadena(String cad, String[] cadena) {
         for (int i = 0; i < cadena.length; i++) {
             if (cadena[i] == null) {
